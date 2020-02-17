4f4b6681ff46c8f430e11a4cc96f18a1176d577e
ddhorstman <davidzilla27@gmail.com>
2020-02-16 21:26:43 -0500

    Stretch: Refactor DRY

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "../index.css"
 import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -24,7 +25,7 @@ const MovieList = props => {
     <div className="movie-list">
       {movies.map(movie => (
         <Link key={movie.id} className="no-decoration" to={`/movies/${movie.id}`} >
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} />
         </Link>
       ))}
     </div>
